356c38392a2b0feee4627dcad7c384f8ae4922bb
Haroon7102 <haroon7102@gmail.com>
2024-10-30 14:33:24 +0500

    after making changes to code

--- instagram-upload-backend/uploadServer.js
+++ instagram-upload-backend/uploadServer.js
@@ -2,9 +2,9 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
-router.get('/auth/callback', async (req, res) => {
+router.get('/callback', async (req, res) => {
     const { code } = req.query;
-
+    console.log('hitting instagram call back')
     try {
         const response = await axios.post('https://api.instagram.com/oauth/access_token', {
             client_id: '1199616704485910',
