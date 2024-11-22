2e4e63bd80236186fa5f2de48f873ee92c1b2b61
Haroon7102 <haroon7102@gmail.com>
2024-11-22 12:52:36 +0500

    getting code done

--- facebook-upload-backend/uploadServer.js
+++ facebook-upload-backend/uploadServer.js
@@ -193,7 +193,7 @@ const uploadVideoToFacebook = async (pageId, accessToken, videoBuffer, filename,
 
 
 async function uploadPhotoToFacebook({ accessToken, pageId, photoBuffer, caption }) {
-    const url = `https://graph.facebook.com/v17.0/${pageId}/photos`;
+    const url = `https://graph.facebook.com/v21.0/${pageId}/photos`;
 
     const formData = new FormData();
     formData.append('access_token', accessToken);
