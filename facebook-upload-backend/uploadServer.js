df16fccc6e2b1a0b5b974b0b4fa8d208370f630e
Haroon7102 <haroon7102@gmail.com>
2024-12-13 14:46:16 +0500

    getting code done

--- facebook-upload-backend/uploadServer.js
+++ facebook-upload-backend/uploadServer.js
@@ -509,25 +509,25 @@ router.post('/upload', upload.array('files', 10), async (req, res) => {
 
                     // Add video ID to mediaIds
                     mediaIds.push(videoResult.id);
-                    postId = videoResult.id; // For video/reel posts, the video itself is the post then
+                    postId = videoResult.id; // For video/reel posts, the video itself is the post
 
                     // Send immediate response to client
+                    savePostToDatabase(email, pageId, pageName, caption, accessToken, mediaIds, postId)
+                        .then(() => {
+                            console.log('Post successfully saved to database.');
+                        })
+                        .catch((dbError) => {
+                            console.error('Error saving post to database:', dbError);
+                        });
+
                     res.json({
                         success: true,
                         postId: postId,
                         message: 'Video upload started. The post will be created shortly.',
                         mediaIds: mediaIds,
                     });
-                    await savePostToDatabase(email, pageId, pageName, caption, accessToken, mediaIds, postId);
 
                     // Save post details in the background (non-blocking)
-                    // savePostToDatabase(email, pageId, pageName, caption, accessToken, mediaIds, postId)
-                    //     .then(() => {
-                    //         console.log('Post successfully saved to database.');
-                    //     })
-                    //     .catch((dbError) => {
-                    //         console.error('Error saving post to database:', dbError);
-                    //     });
 
                 } catch (error) {
                     console.error('Error during video upload:', error);
