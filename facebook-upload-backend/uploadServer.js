eac139fb3b9977f0fef833330cb39d0b0fdfa288
Haroon7102 <haroon7102@gmail.com>
2025-01-09 22:32:22 +0500

    resolving error

--- facebook-upload-backend/uploadServer.js
+++ facebook-upload-backend/uploadServer.js
@@ -542,7 +542,8 @@ router.post('/upload', upload.array('files', 10), async (req, res) => {
 });
 
 
-//this is s ahbhdfiufiurefhfhuerherfhieriheuruhuireh4444444444444444433555555
+
+//this is s ahbhdfiufiurefhfhuerherfhieriheuruhuireh4444444444444444433555555how to do this
 
 
 // Route to upload files and post to Facebook
